23795c9a8a4a94f52f46674058d1cf56f7b38694
andru <andru299@gmail.com>
2023-06-29 18:46:19 -0500

    cambios clase 16 jun

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,82 @@
+import React, {useEffect, useState} from 'react'
+import styled from "@emotion/styled";
+import useSelectMoneda from '../hooks/useSelectMoneda';
+import {monedas} from '../data/monedas'
+import Error from './Error';
+
+const InputSubmit= styled.input`
+    background-color: #9497ff;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #fff;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    margin-top: 30px;
+    transition: background-color .3s ease;
+    &:hover{
+      background-color: #7a7dfe;
+      cursor: pointer;
+    }
+`;
+
+const Formulario = ({setMonedas}) => {
+
+  const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
+  const [moneda, SelectMonedas] = useSelectMoneda('Elige tu moneda', monedas);
+  const [criptomoneda, SelectCriptoMoneda] = useSelectMoneda ('Elige tu CriptoMoneda', criptos);
+
+ 
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url= "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      const arrayCriptos = resultado.Data.map(cripto => {
+        const objeto = {
+        id: cripto.CoinInfo.Name,
+        nombre: cripto.CoinInfo.FullName
+        }
+        return objeto;
+        })
+
+        setCriptos(arrayCriptos);
+        
+  
+      }
+      consultarAPI();
+    }, []);
+
+    const handleSubmit = e =>{
+      e.preventDefault();
+
+      if([moneda, criptomoneda].includes('')){
+        setError(true);
+        return;
+      }
+      setError(false);
+      setMonedas ({
+        moneda,
+        criptomoneda
+      })
+    }
+    
+  return (
+    <>
+    {error && <Error>Todos los campos son obligatorios</Error>}
+    
+    <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptoMoneda />
+        <InputSubmit type='submit' value="cotizar" />
+    </form>
+    </>
+  )
+}
+
+
+export default Formulario;
